0492be29fc0676e8e0003ebcf8ffa3605d3ccbe3
Jan van Dillen <janvandillen1991@gmail.com>
2021-04-09 12:20:09 +0200

    Spring boot init

---
+++ src/main/java/nl/jvandillen/slackbotateteen/SlackBotAteteenApplication.java
@@ -0,0 +1,13 @@
+package nl.jvandillen.slackbotateteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackBotAteteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackBotAteteenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/jvandillen/slackbotateteen/SlackBotAteteenApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.jvandillen.slackbotateteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackBotAteteenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
